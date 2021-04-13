d9b586494dc1cd101a466b438906faff61f21083
camsdono <68128526+camsdono@users.noreply.github.com>
2021-04-13 16:56:35 +0100

    Add files via upload

---
+++ Assets/Scripts/Player/plr_Movement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class plr_Movement : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
